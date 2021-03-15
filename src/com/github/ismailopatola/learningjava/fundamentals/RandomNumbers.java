f2dc5c248b76bf777e6b02852799465698d4f22c
Ismail Opatola <opatolamails@gmail.com>
2021-03-15 04:42:24 +0100

    using random lib

---
+++ src/com/github/ismailopatola/learningjava/fundamentals/RandomNumbers.java
@@ -0,0 +1,16 @@
+package com.github.ismailopatola.learningjava.fundamentals;
+import java.util.Random;
+
+public class RandomNumbers {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		int x = random.nextInt(6);
+		double y = random.nextDouble();
+		boolean z = random.nextBoolean();
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+
+}
