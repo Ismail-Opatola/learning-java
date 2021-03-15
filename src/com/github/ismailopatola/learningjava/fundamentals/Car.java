e5d0557e9e129117ca1236430d8240fc14a81abd
Ismail Opatola <opatolamails@gmail.com>
2021-03-15 18:14:44 +0100

    object OOP

---
+++ src/com/github/ismailopatola/learningjava/fundamentals/Car.java
@@ -0,0 +1,17 @@
+package com.github.ismailopatola.learningjava.fundamentals;
+
+public class Car {
+	String make = "Chevrolet";
+	String model = "Corvette";
+	int year = 2021;
+	String color = "blue";
+	double price = 500000.00;
+	
+	void drive () {
+		System.out.println("You drive the car");
+	}
+	
+	void brake() {
+		System.out.println("You step on the brake");
+	}
+}
---
+++ src/com/github/ismailopatola/learningjava/fundamentals/ObjectsOOP.java
@@ -0,0 +1,32 @@
+package com.github.ismailopatola.learningjava.fundamentals;
+
+public class ObjectsOOP {
+
+	public static void main(String[] args) {
+		/**
+		 * Object (OOP)
+		 * ------------
+		 * + object is an instance of a class that may contain <i>attributes</i> and <i>methods</i>
+		 * + ex. (phone, desk, computer, coffee cup)
+		 */
+		
+		// construct an instance
+		Car myCar1 = new Car();
+		Car myCar2 = new Car();
+		
+		System.out.println(myCar1.make);
+		System.out.println(myCar1.model);
+		System.out.println(myCar1.price);
+		myCar1.drive();
+		myCar1.brake();
+
+		System.out.println();
+
+		System.out.println(myCar2.make);
+		System.out.println(myCar2.model);
+		System.out.println(myCar2.price);
+		myCar2.drive();
+		myCar2.brake();
+	}
+
+}
