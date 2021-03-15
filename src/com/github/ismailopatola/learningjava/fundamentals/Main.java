b3ebfb48d20f683eaff8dc4ac6eee5a8eaea3197
Ismail Opatola <opatolamails@gmail.com>
2021-03-15 04:40:19 +0100

    intro to JDK, comments, logging, data-types

---
+++ src/com/github/ismailopatola/learningjava/fundamentals/Main.java
@@ -0,0 +1,80 @@
+package com.github.ismailopatola.learningjava.fundamentals;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("I love pizza"); // single line print 
+		System.out.print("It's really good! \n"); // in-line print
+		
+		// single line comment
+		
+		/**
+		 * <b>Multi</b>-line
+		 * comment
+		 * */
+		
+		/*
+		 * ==============
+		 * DATA TYPES
+		 * ==============
+		 * 
+		 * data type 
+		 * 		-> size 
+		 * 		-> primitive/reference 
+		 * 		-> value
+		 * 1. boolean 
+		 * 		-> 1 bit 
+		 * 		-> primitive 
+		 * 		-> true or false
+		 * 2. byte  
+		 * 		-> 1 bit 
+		 * 		-> primitive 
+		 * 		-> -128 to 127
+		 * 3. short 
+		 * 		-> 2 bytes 
+		 * 		-> primitive 
+		 * 		-> -32,768 to 32,767
+		 * 4. int 
+		 * 		-> 4 bytes 
+		 * 		-> primitive 
+		 * 		-> -2 billion to 2 billion
+		 * 5. long 
+		 * 		-> 8 bytes 
+		 * 		-> primitive 
+		 * 		-> -9 quintillion to 9 quintillion
+		 * 6. float 
+		 * 		-> 4 bytes 
+		 * 		-> primitive
+		 * 		-> fractional number up to 6-7 digits - e.g. 3.141592f
+		 * 7. double 
+		 * 		-> 8 bytes 
+		 * 		-> primitive
+		 * 		-> fractional number up to 15 digits - e.g. 3.141592653589793 
+		 * 8. char
+		 * 		-> 2 bytes
+		 * 		-> primitive
+		 * 		-> single character/letter/ASCII value - e.g. 'f'
+		 * 9. String
+		 * 		-> varies
+		 * 		-> reference
+		 * 		-> a sequence of characters - e.g "Hello world!"
+		 * */
+
+		/*
+		 * ===========
+		 * Compiler
+		 * ============
+		 * 
+		 * java complier - transform source code to machine code
+		 * JVM - Translate byte code to machine code
+		 * byte code - is cross-platform
+		 * Source Code (.java) | byte code (.class) | object code - (machine code 10111010100101)
+		 * 
+		 * JDK - Java Development Kit (developer tool)
+		 * + JRE - Java Runtime Environment (contains libraries & toolkits)
+		 *  + JVM - Java Virtual Machine (runs java programs)
+		 */
+	}
+
+}
