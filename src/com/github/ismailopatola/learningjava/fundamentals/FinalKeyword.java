6f1d30936caa08f19840247031904d259a423045
Ismail Opatola <opatolamails@gmail.com>
2021-03-15 18:14:21 +0100

    final keyword

---
+++ src/com/github/ismailopatola/learningjava/fundamentals/FinalKeyword.java
@@ -0,0 +1,21 @@
+package com.github.ismailopatola.learningjava.fundamentals;
+
+public class FinalKeyword {
+
+	public static void main(String[] args) {
+		/**
+		 * Final Keyword in Java
+		 * ---------------------
+		 * similar to javascript const
+		 * anything decalared as final can not be changed later in the program
+		 */
+		
+		final double PI = 3.14159; // uppercase naming convension
+		
+//		PI = 4; // error
+		
+		System.out.println(PI);
+
+	}
+
+}
