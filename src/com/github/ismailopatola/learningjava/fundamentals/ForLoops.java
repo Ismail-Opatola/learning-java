8b7aa0ae35d71a49ddba5c3976f5773ab16d69c7
Ismail Opatola <opatolamails@gmail.com>
2021-03-15 16:31:07 +0100

    for loop

---
+++ src/com/github/ismailopatola/learningjava/fundamentals/ForLoops.java
@@ -0,0 +1,15 @@
+package com.github.ismailopatola.learningjava.fundamentals;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		
+		// decreament by 2
+		for(int i=10; i>=0; i-=2) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Happy new year");
+	}
+
+}
