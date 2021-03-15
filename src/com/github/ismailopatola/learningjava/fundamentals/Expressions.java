4e0cfaf6a4837c7f1a738a60df6fe646c0bfd4f0
Ismail Opatola <opatolamails@gmail.com>
2021-03-15 04:41:32 +0100

    expressions

---
+++ src/com/github/ismailopatola/learningjava/fundamentals/Expressions.java
@@ -0,0 +1,14 @@
+package com.github.ismailopatola.learningjava.fundamentals;
+
+public class Expressions {
+
+	public static void main(String[] args) {
+		// expression = operands & operators
+		// operands = values, variables, number, quality
+		// operators = + - * / %
+		double friends = 10;
+		friends = (double) friends / 3;
+		System.out.println(friends);
+	}
+
+}
