dfe41343c153503ff28a6ded6e0f136109eb2e52
Ismail Opatola <opatolamails@gmail.com>
2021-03-19 11:52:28 +0100

    File writer class

---
+++ src/com/github/ismailopatola/learningjava/fundamentals/FileWriterClass.java
@@ -0,0 +1,30 @@
+package com.github.ismailopatola.learningjava.fundamentals;
+
+import java.io.FileWriter;
+
+public class FileWriterClass {
+
+	public static void main(String[] args) {
+		/**
+		 * using File writer to write to a file
+		 * ------------------------------------
+		 * 
+		 */
+
+		try {
+			FileWriter writer = new FileWriter("poem.txt");
+
+			// overide content of file
+			writer.write("Roses are red \nViolets are blue\nI'm stuck on you!");
+			
+			// add new content at the end of file content
+			writer.append("\n(A peom by Bro)");
+			writer.
+			writer.close();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
