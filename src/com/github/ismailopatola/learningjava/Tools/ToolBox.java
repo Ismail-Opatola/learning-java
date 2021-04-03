8b22d91b098239d42a12525c6f1194e9ffb3692a
Ismail Opatola <opatolamails@gmail.com>
2021-04-03 15:19:28 +0100

    packages

---
+++ src/com/github/ismailopatola/learningjava/Tools/ToolBox.java
@@ -0,0 +1,5 @@
+package com.github.ismailopatola.learningjava.Tools;
+
+public class ToolBox {
+
+}
---
+++ src/com/github/ismailopatola/learningjava/fundamentals/Packages.java
@@ -0,0 +1,18 @@
+package com.github.ismailopatola.learningjava.fundamentals;
+
+import javax.swing.Icon;
+
+import com.github.ismailopatola.learningjava.Tools.ToolBox;
+
+public class Packages {
+	public static void main(String[] args) {
+		/**
+		 * Packages in java
+		 * ----------------
+		 * a bundle of rel files that we can use in our program
+		 */
+		
+		ToolBox toolBox = new ToolBox();
+		Icon icon;
+	}
+}
